beb26e40fb787f65c575657cc48f2d1632f31355
hugojohansson <carlhugojohansson@outlook.com>
2023-05-16 00:05:07 +0200

    made som script chages, to size moviecard

--- script.js
+++ script.js
@@ -18,40 +18,64 @@ async function fetchData(title) {
     }
 }
 
+
+
+
+
+function calculateCardsPerRow() {
+    if (window.innerWidth < 768) {
+      return 1; // For small screens, show one card per row
+    } else if (window.innerWidth < 992) {
+      return 2; // For medium screens, show two cards per row
+    } else if (window.innerWidth < 1200) {
+      return 3; // For large screens, show three cards per row
+    } else {
+      return 4; // For extra-large screens, show four cards per row
+    }
+  }
+
+
+
+// Function to display the movie cards
 function displayResults(results) {
     const searchResults = document.getElementById('search-results');
     searchResults.innerHTML = '';
-
+  
+    const cardsPerRow = calculateCardsPerRow();
+    const cardWidth = 100 / cardsPerRow;
+  
     results.forEach((result) => {
-        const { title, img, vtype, synopsis, clist } = result;
-
-        const resultContainer = document.createElement('div');
-        resultContainer.classList.add('result');
-
-        const imgElement = document.createElement('img');
-        imgElement.src = img;
-
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = title;
-
-        const vtypeElement = document.createElement('p');
-        vtypeElement.textContent = `Type: ${vtype}`;
-
-        const clistElement = document.createElement('p');
-        clistElement.textContent = `Available in: ${clist}`;
-
-        const synopsisElement = document.createElement('p');
-        synopsisElement.textContent = synopsis;
-
-        resultContainer.appendChild(imgElement);
-        resultContainer.appendChild(titleElement);
-        resultContainer.appendChild(vtypeElement);
-        resultContainer.appendChild(clistElement);
-        resultContainer.appendChild(synopsisElement);
-
-        searchResults.appendChild(resultContainer);
+      const { title, img, vtype, synopsis, clist } = result;
+  
+      const resultContainer = document.createElement('div');
+      resultContainer.classList.add('movie-item');
+      resultContainer.style.width = `${cardWidth}%`;
+  
+      const imgElement = document.createElement('img');
+      imgElement.src = img;
+  
+      const titleElement = document.createElement('h2');
+      titleElement.textContent = title;
+  
+      const vtypeElement = document.createElement('p');
+      vtypeElement.textContent = `Type: ${vtype}`;
+  
+      const synopsisElement = document.createElement('p');
+      synopsisElement.textContent = synopsis;
+  
+      const clistElement = document.createElement('p');
+      clistElement.textContent = `Available in: ${clist}`;
+  
+      resultContainer.appendChild(imgElement);
+      resultContainer.appendChild(titleElement);
+      resultContainer.appendChild(vtypeElement);
+      resultContainer.appendChild(synopsisElement);
+      resultContainer.appendChild(clistElement);
+  
+      searchResults.appendChild(resultContainer);
     });
-}
+  }
+  
 
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-button');
