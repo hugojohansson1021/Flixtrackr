3f2a387cd64664d40b394a2b3268485007fea3b9
hugojohansson <carlhugojohansson@outlook.com>
2023-05-28 11:55:07 +0200

    updt

--- individual.js
+++ individual.js
@@ -67,15 +67,28 @@ function displayMovieDetails(movie, availData) {
 
   const clistElement = document.createElement('p');
   const availableCountries = availData.results.map(country => country.country);
-  clistElement.textContent = `Available in:-> ${availableCountries.join(', ')}`;
 
-  const emptySpaceElement = document.createElement('p');
-  emptySpaceElement.innerHTML = '&nbsp;'; // Add an empty space using HTML entity
-
-  const emptySpaceElements = document.createElement('p');
-  emptySpaceElement.innerHTML = '&nbsp;'; // Add an empty space using HTML entity
+  const availableInText = document.createElement('span');
+  availableInText.textContent = 'Available in : ';
+  clistElement.appendChild(availableInText);
 
 
+  availableCountries.forEach(country => {
+    const flagElement = document.createElement('span');
+    const countryCode = getCountryCode(country);
+    flagElement.classList.add('flag-icon', `flag-icon-${countryCode}`);
+
+    const countryNameElement = document.createElement('span');
+    countryNameElement.textContent = country;
+
+    const countryContainer = document.createElement('span');
+    countryContainer.style.display = 'flex';
+    countryContainer.style.alignItems = 'center';
+    countryContainer.appendChild(flagElement);
+    countryContainer.appendChild(countryNameElement);
+
+    clistElement.appendChild(countryContainer);
+  });
 
   movieDetails.appendChild(imgElement);
   movieDetails.appendChild(titleElement);
@@ -85,13 +98,66 @@ function displayMovieDetails(movie, availData) {
   movieDetails.appendChild(directorElement);
   movieDetails.appendChild(castElement);
   movieDetails.appendChild(clistElement);
-  movieDetails.appendChild(emptySpaceElement); 
-  movieDetails.appendChild(emptySpaceElements); //
 
   // Center the content
   movieDetails.style.display = 'flex';
   movieDetails.style.flexDirection = 'column';
   movieDetails.style.alignItems = 'center';
+}
+
+function getCountryCode(country) {
+  const countryCodeMap = {
+    // Add more country code mappings as needed
+    'India ': 'IN',
+    'Israel ': 'IL',
+    'Lithuania ': 'LT',
+    'Brazil ': 'BR',
+    'Slovakia ': 'SK',
+    'Spain ': 'ES',
+    'United Kingdom ': 'GB',
+    'Romania ': 'RO',
+    'Argentina ': 'AR',
+    'Canada ': 'CA',
+    'Iceland ': 'IS',
+    'Mexico ': 'MX',
+    'Hong Kong ': 'HK',
+    'Belgium ': 'BE',
+    'United States ': 'US',
+    'Switzerland ':'CH',
+    'Greece ':'GR',
+    'Czech Republic ': 'CZ',
+    'Australia ': 'AU',
+    'South Korea ': 'KR',
+    'France ': 'FR',
+    'Japan ': 'JP',
+    'Poland ': 'PL',
+    'Russia ': 'RU',
+    'Netherlands ': 'NL',
+    'Sweden ': 'SE',
+    'Singapore ': 'SG',
+    'Germany ': 'DE',
+    'Portugal ': 'PT',
+    'South Africa ': 'ZA',
+    'Italy ': 'IT',
+    'Thailand ': 'TH',
+    'Hungary ': 'HU',
+    'Turkey ': 'TR',
+    'Malaysia ': 'MY',
+    'Colombia ': 'CO',
+    'Philippines ': 'PH',
+    'Ukraine ': 'UA',
+
+
+ 
+    // ...
+  };
+
+  const countryCode = countryCodeMap[country];
+  if (countryCode) {
+    return countryCode;
+  }
+
+  return 'unknown';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
